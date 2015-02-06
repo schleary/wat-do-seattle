cb80a63df0cadfa4b0b40850a87dd952b8ce4129
Holly Leary <schleary@gmail.com>
2015-02-06 11:53:34 -0800

    s3 working

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     provider:               'AWS',
     aws_access_key_id:      ENV['S3_KEY'],
     aws_secret_access_key:  ENV['S3_SECRET'],
-
+    region:                 "us-west-2"
   }
 
   # For testing, upload files to local `tmp` folder.
