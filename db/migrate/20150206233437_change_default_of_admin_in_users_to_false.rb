c408e6cabe72a40764087a500746a36a072c6b93
Holly Leary <schleary@gmail.com>
2015-02-06 15:39:30 -0800

    made admin dafault false migration

---
+++ db/migrate/20150206233437_change_default_of_admin_in_users_to_false.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultOfAdminInUsersToFalse < ActiveRecord::Migration
+  def change
+    change_column_default :users, :admin, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206225520) do
+ActiveRecord::Schema.define(version: 20150206233437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20150206225520) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.boolean  "admin",            default: false
     t.string   "uid"
     t.string   "image_url"
     t.string   "oauth_token"
