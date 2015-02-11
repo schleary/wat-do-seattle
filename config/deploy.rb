feef912e93677c3a4375df58086bb08b42152116
Holly Leary <schleary@gmail.com>
2015-02-10 16:12:49 -0800

    deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'dotenv'
 Dotenv.load
-# config valid only for current version of Capistrano
+f# config valid only for current version of Capistrano
 lock '3.3.5'
 
 set :application, 'wat-do-seattle'
@@ -30,6 +30,7 @@ set :deploy_to, '/var/www/wat-do-seattle'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
