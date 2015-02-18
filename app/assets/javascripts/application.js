75c664a19bc360ed81df94bfd45b01dd31a1e6d2
Holly Leary <schleary@gmail.com>
2015-02-18 09:11:14 -0800

    safe point before embarking on capistrano

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,46 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$( document ).ready(function() {
+  $('#slider-snap').noUiSlider({
+    start: [ 1, 3 ],
+    snap: true,
+    connect: true,
+    range: {
+      'min': 0,
+      '20%': 1,
+      '40%': 2,
+      '60%': 3,
+      '80%': 4,
+      'max': 5
+    }
+  });
+
+  $('#slider-snap').Link('lower').to(function(minSliderValue) {
+    var values = ['Inactive (ex: Watching a Movie)',
+                  'Some Mental Activity (ex: Reading a Book)',
+                  'Mentally Active (ex: Playing Chess)',
+                  'Moderate (ex: Light Yoga)',
+                  'Vigorous (ex: Running)',
+                  'Extreme (ex: Mountain Climbing)',
+                  'Dont Care (Surprise Me!)'];
+
+    $("#min_activity_level").attr("value", parseInt(minSliderValue));
+     $('#slider-snap-value-lower').html(values[parseInt(minSliderValue)]);
+  });
+
+  $('#slider-snap').Link('upper').to(function(maxSliderValue) {
+    var values = ['Inactive (ex: Watching a Movie)',
+                  'Some Mental Activity (ex: Reading a Book)',
+                  'Mentally Active (ex: Playing Chess)',
+                  'Moderate (ex: Light Yoga)',
+                  'Vigorous (ex: Running)',
+                  'Extreme (ex: Mountain Climbing)',
+                  'Dont Care (Surprise Me!)'];
+
+    $("#max_activity_level").attr("value", parseInt(maxSliderValue));
+     $('#slider-snap-value-upper').html(values[parseInt(maxSliderValue)]);
+  });
+
+});
