72d393326d1a07cdba6c5cc9416f6d46a4afc0a9
Holly Leary <schleary@gmail.com>
2015-02-18 19:28:22 -0800

    still trying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 25,
+    port:                 465,
     domain:               'gmail.com',
     user_name:            ENV["MAIL_USERNAME"], # your gmail login
     password:             ENV["MAIL_PASSWORD"], # your gmail password
