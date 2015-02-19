619d1ab7920b57509841b3c320ac8c1a24659644
Holly Leary <schleary@gmail.com>
2015-02-18 19:18:45 -0800

    trying to get resque to work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'gmail.com',
     user_name:            ENV["MAIL_USERNAME"], # your gmail login
     password:             ENV["MAIL_PASSWORD"], # your gmail password
     authentication:       'plain',
