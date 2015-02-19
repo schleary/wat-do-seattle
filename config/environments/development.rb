ec4db09733f6a44c3806594761cbc70a42a5b127
Holly Leary <schleary@gmail.com>
2015-02-18 19:34:49 -0800

    removed domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 465,
-    domain:               'wat-do-seattle.com',
     user_name:            ENV["MAIL_USERNAME"], # your gmail login
     password:             ENV["MAIL_PASSWORD"], # your gmail password
     authentication:       'plain',
